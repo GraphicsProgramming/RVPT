f3ed9ec71dd4490611e244b2db776c1e31710e85
Alon Regev <aregevdev@gmail.com>
2020-04-23 22:14:43 +0300

    Add GLFW glm nlohmann-json stb_image tinyobjloader vk-bootstrap

--- src/rvpt/main.cpp
+++ src/rvpt/main.cpp
@@ -2,10 +2,20 @@
 // Created by AregevDev on 23/04/2020.
 //
 
-#include <iostream>
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    std::cout << "Hello RVPT" << '\n';
+    glfwInit();
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+    GLFWwindow *window = glfwCreateWindow(500, 500, "RVPT", nullptr, nullptr);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
