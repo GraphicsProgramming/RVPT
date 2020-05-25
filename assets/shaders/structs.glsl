3c7bb28db44b19b539dc1d0940d2ea80cadf2faf
LegendWasTaken <caiodcw@gmail.com>
2020-05-25 13:57:34 +0700

    Add basic RT compute

---
+++ assets/shaders/structs.glsl
@@ -0,0 +1,16 @@
+struct Sphere {
+    vec3 origin;
+    float radius;
+};
+
+struct Ray {
+    vec3 origin;
+    vec3 direction;
+};
+
+struct Camera {
+    vec3 origin;
+    vec3 center;
+    vec3 horizontal;
+    vec3 vertical;
+};
--- scripts/compile_shaders.sh
+++ scripts/compile_shaders.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for filename in *.{vert,frag,tesc,tese,geom,comp}; do
     [ -e "$filename" ] || continue
-    glslangValidator -V "$filename" -o "$filename.spv"
+    glslangValidator -V "$filename" -o "$filename.spv" -I"."
 done
