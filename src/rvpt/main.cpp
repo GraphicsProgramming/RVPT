4135e6538cc799a881ef5bcde22e9dcc5376232d
LegendWasTaken <caiodcw@gmail.com>
2020-05-29 09:37:34 +0700

    Fix rebase conflicts

--- src/rvpt/main.cpp
+++ src/rvpt/main.cpp
@@ -22,20 +22,13 @@ int main()
         return 0;
     }
 
-    window.add_key_callback([&](int keycode, Window::Action action) {
-        if (keycode == GLFW_KEY_ESCAPE && action == Window::Action::RELEASE)
-        {
-            window.set_close();
-        }
-        if (keycode == GLFW_KEY_R && action == Window::Action::RELEASE)
-        {
-            rvpt.reload_shaders();
-        }
-    });
-
     while (!window.should_close())
     {
         window.poll_events();
+        if (window.is_key_down(Window::KeyCode::KEY_ESCAPE))
+            window.set_close();
+        if(window.is_key_down(Window::KeyCode::KEY_R))
+            rvpt.reload_shaders();
         rvpt.update();
         rvpt.draw();
     }
