f39782c36cad7f1be1d4fd00461f3c29b5ad4c16
LegendWasTaken <caiodcw@gmail.com>
2020-06-21 08:58:25 +0800

    Oops

--- assets/shaders/util.glsl
+++ assets/shaders/util.glsl
@@ -1,6 +1,6 @@
 Ray get_ray(vec2 pixel)
 {
-    vec4 origin = cam.matrix * vec4(cam.origqin, 1);
+    vec4 origin = cam.matrix * vec4(cam.origin, 1);
     vec4 direction =
         cam.matrix *
         vec4(normalize((cam.center + cam.horizontal * pixel.x + cam.vertical * pixel.y) -
