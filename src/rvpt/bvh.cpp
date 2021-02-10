b3a5215e6ab19fbe64bf57aa6c73e131b3ca5f56
Arsène Pérard-Gayot <arsene.perard@laposte.net>
2021-02-10 17:29:38 +0100

    Add missing files

---
+++ src/rvpt/bvh.cpp
@@ -0,0 +1,20 @@
+﻿//
+// Created by legend on 20/8/2020.
+//
+
+#include "bvh.h"
+
+void Bvh::collect_aabbs_by_depth(
+    std::vector<std::vector<AABB>>& aabbs,
+    size_t depth, size_t node_index) const
+{
+    const BvhNode& node = nodes[node_index];
+    if (depth >= aabbs.size())
+        aabbs.resize(depth + 1);
+    aabbs[depth].push_back(node.aabb());
+    if (!node.is_leaf())
+    {
+        collect_aabbs_by_depth(aabbs, depth + 1, node.first_child_or_primitive + 0);
+        collect_aabbs_by_depth(aabbs, depth + 1, node.first_child_or_primitive + 1);
+    }
+}
