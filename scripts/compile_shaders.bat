67fda776979a5b0db63d4d38dfeed1506d5fefc7
tigrazone <tigrazone@gmail.com>
2021-11-06 09:22:19 +0200

    optimize refraction
    rewrite intersect_scene_any with bvh - it fixed integrator_binary, integrator_ao, integrator_Appel, integrator_Whitted

--- scripts/compile_shaders.bat
+++ scripts/compile_shaders.bat
@@ -1,2 +1,2 @@
 @echo off
-for %%i in (*.vert *.frag *.comp) do "glslangValidator.exe" -V "%%~i" -o "%%~i.spv" -I"."
+for %%i in (*.vert *.frag *.comp) do "%VULKAN_SDK%\bin\glslangValidator.exe" -V "%%~i" -o "%%~i.spv" -I"."
